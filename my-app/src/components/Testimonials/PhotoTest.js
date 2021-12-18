e3818a8437b71ce13b5a22c5c99aacc6164f8cfe
anjaliskalange <anjaliskalange@gmail.com>
2021-12-18 15:32:21 +0530

    photoTest editing

--- my-app/src/components/Testimonials/PhotoTest.js
+++ my-app/src/components/Testimonials/PhotoTest.js
@@ -6,9 +6,14 @@ function PhotoTest(props) {
     <div className="main">
       <div className="photoTest">
         <div className="main-container">
-          <div><img src={desktop} className='main-container__image'></img></div>
-          <div className="main-container__name">{props.name}</div>
-          <div className="main-container__text">{props.text}</div>
+          <div className="container">
+            <img src={desktop} className='main-container__image'></img>
+            <div className="main-container__name">{props.name}</div>
+          </div>
+          <div className="overlay">
+            <div className="main-container__text">{props.text}</div>
+            <div className="main-container__rating">{props.rating}⭐️</div>
+          </div>
         </div>
       </div>
     </div>
--- my-app/src/components/Testimonials/PhotoTestContainer.js
+++ my-app/src/components/Testimonials/PhotoTestContainer.js
@@ -7,17 +7,20 @@ function PhotoTestContainer(){
         {
             id:'p1',
             name:'Sejal Palkar',
-            text:"Hey it's Sejal I have done the Accounts and Finance and I'm happy to say that it was very useful in my day to day job as I'm working at a CA firm. I really liked the regularity in everyday lectures conducted and also the excess knowledge is given by the tutor outside the course syllabus So overall I would like to give it 4.5 stars😊",
+            text:"Hey it's Sejal I have done the Accounts and Finance and I'm happy to say that it was very useful in my day to day job as I'm working at a CA firm. I really liked the regularity in everyday lectures conducted and also the excess knowledge is given by the tutor outside the course syllabus.",
+            rating:'4.5/5',
         },
         {
             id:'p2',
             name:'Bharat Kamble',
-            text:"I understand what you have taught me.The notes you provide to us are very useful.You do not need to make any changes in the course. You teach perfectly.What I have learned is very useful in the CA firm internship you have given.I want to give you 5 out of 5 stars.",
+            text:"I understand what you have taught me.The notes you provide to us are very useful.You do not need to make any changes in the course. You teach perfectly.What I have learned is very useful in the CA firm internship you have given.",
+            rating:'5/5',
         },
         {
             id:'p3',
             name:'Prashant',
             text:"The information I got was helpful for me… a way of teaching is good....N ur notes are also goodyour course will help me for getting jobs n internships",
+            rating:'4.5/5',
         
         }
 
@@ -28,6 +31,7 @@ function PhotoTestContainer(){
             {images.map((item) => (
                         <PhotoTest name={item.name}
                                    text={item.text}
+                                   rating={item.rating}
                         />
                     ))}
             
