b88f73b1d2a84443f308bae820adf91b520ac567
Sanket1032 <sanketshinde1032@gmail.com>
2021-12-22 13:14:01 +0530

    faq_minor

--- my-app/src/components/ContactUs/FaqQuestion.js
+++ my-app/src/components/ContactUs/FaqQuestion.js
@@ -1,11 +1,27 @@
-function FaqQuestion (props) {
-    return(
+import { React, useState } from 'react';
+
+function FaqQuestion(props) {
+
+    const [active, setActive] = useState('false');
+    const faq_answer = document.querySelector('.faq__answer');
+    const setActiveHandler = () => {
+        if (active === 'true') {
+            setActive('true');
+            faq_answer.classList.remove('faq_answer');
+        }
+        else {
+            setActive('false');
+            faq_answer.classList.add('faq_answer');
+        }
+    }
+
+    return (
         <div class="faq__questions" id={props.id}>
             <div class="faq__question">
                 <span>{props.question}</span>
                 <div class="faq__show">+</div>
             </div>
-            <div class="faq__answer">{props.answer}</div>
+            <div class="faq__answer" onClick={setActiveHandler}>{props.answer}</div>
         </div>
     );
 };
