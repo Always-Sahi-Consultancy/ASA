a96a091c62f8ec4b861e3b36d82a04f1eeff4aa8
Sanket1032 <sanketshinde1032@gmail.com>
2021-12-22 14:36:00 +0530

    faq correction done

--- my-app/src/components/ContactUs/FaqQuestion.js
+++ my-app/src/components/ContactUs/FaqQuestion.js
@@ -2,27 +2,17 @@ import { React, useState } from 'react';
 
 function FaqQuestion(props) {
 
-    const [active, setActive] = useState('false');
-    const faq_answer = document.querySelector('.faq__answer');
-    const setActiveHandler = () => {
-        if (active === 'true') {
-            setActive('true');
-            faq_answer.classList.remove('faq_answer');
-        }
-        else {
-            setActive('false');
-            faq_answer.classList.add('faq_answer');
-        }
-    }
+    const [active, setActive] = useState(false);
+
 
     return (
         <div class="faq__questions" id={props.id}>
             <div class="faq__question">
                 <span>{props.question}</span>
-                <div class="faq__show">+</div>
+                <div class="faq__show" onClick={() => setActive(!active)}>+</div>
             </div>
-            <div class="faq__answer" onClick={setActiveHandler}>{props.answer}</div>
-        </div>
+            <div class="faq__answer" style={active ? { display: "block" } : { display: "none" }}>{props.answer}</div>
+        </div >
     );
 };
 
