ea76fcbfb87a74ebedbdd1f1f08596a75caaa53c
Sanket1032 <sanketshinde1032@gmail.com>
2021-12-24 18:11:17 +0530

    faq minor changes

--- my-app/src/components/ContactUs/FaqQuestion.js
+++ my-app/src/components/ContactUs/FaqQuestion.js
@@ -1,14 +1,23 @@
 import { React, useState } from 'react';
 
 function FaqQuestion(props) {
-
+    const faq__show = document.querySelector(".faq__show");
     const [active, setActive] = useState(false);
+    const faq_work = () => {
+        setActive(!active);
+        // if (active === "false") {
+        //     faq__show.innerHTML = "+";
+        // }
+        // else {
+        //     faq__show.innerHTML = "-";
+        // }
+    }
 
     return (
         <div class="faq__questions" id={props.id}>
             <div class="faq__question">
                 <span>{props.question}</span>
-                <div class="faq__show" onClick={() => setActive(!active)}>+</div>
+                <div class="faq__show" onClick={faq_work}>+</div>
             </div>
             <div class="faq__answer" style={active ? { display: "block" } : { display: "none" }}>{props.answer}</div>
         </div >
