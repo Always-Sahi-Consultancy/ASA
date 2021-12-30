d4db739dd1e5c453f57efa8a8347b40d175ff944
anjaliskalange <anjaliskalange@gmail.com>
2021-12-31 01:56:55 +0530

    explore new courses

--- my-app/src/components/Landing/Explore New/ExploreNew.js
+++ my-app/src/components/Landing/Explore New/ExploreNew.js
@@ -1,10 +1,54 @@
 import './ExploreNew.css';
 import NewCourses from './NewCourses';
+import Code from '../../../image/code.png';
+import Apple from '../../../image/Apple.png';
+import Graph from '../../../image/g.png';
+import Pencil from '../../../image/pencil.png';
+import Video from '../../../image/videoclap.png';
+import Tax from '../../../image/tax.png'; 
+import Money from '../../../image/money.png';
 
 function ExploreNew(){
+    const new_Courses = [
+        {
+            id:'1',
+            Text:'Programming and Tech',
+            Image:Code
+        },
+        {
+            id:'2',
+            Text:"Graphic designing",
+            Image:Pencil
+        },
+        {
+            id:'3',
+            Text:"taxation",
+            Image:Tax
+        },
+        {
+            id:'4',
+            Text:"Video editing and animation",
+            Image:Video
+        },
+        {
+            id:'5',
+            Text:"Marketing",
+            Image:Graph
+        },
+        {
+            id:'6',
+            Text:"Finance",
+            Image:Money
+        }
+    ]
     return<div className='NewContainer'>
         <div className='NewContainer__head'>EXPLORE NEW COURSES</div>
-        <div><NewCourses/></div>
+        
+        {new_Courses.map((item) => (
+                        <NewCourses Text={item.Text}
+                                   Image={item.Image}
+                        />
+                    ))}
     </div>
 }
 
--- my-app/src/components/Landing/Explore New/NewCourses.js
+++ my-app/src/components/Landing/Explore New/NewCourses.js
@@ -1,9 +1,15 @@
 import './NewCourses.css';
 
 
-function NewCourses(){
+function NewCourses(props){
+    
     return<div>
-        <div className='imgs'></div>
+        <div className='container'>
+                <div className='content'>
+                    <div className='element'><img src={props.Image} className='element_img'></img></div>
+                    <div className='element_name'>{props.Text}</div>
+                </div>
+        </div>
     </div>
 }
 
