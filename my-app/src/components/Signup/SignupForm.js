8c852c48d17d2a7a781d70e208d0ac4cf541b1c2
Akhil-A0402 <akhil.a.nair760@gmail.com>
2021-12-31 15:59:23 +0530

    Signup Component

---
+++ my-app/src/components/Signup/SignupForm.js
@@ -0,0 +1,10 @@
+const SignUpForm = () => {
+    return (
+        <form method="post" action="#">
+            <input placeholder="Enter your Email" />
+            <button type="submit">Continue</button>
+        </form>
+    );
+}
+
+export default SignUpForm;
