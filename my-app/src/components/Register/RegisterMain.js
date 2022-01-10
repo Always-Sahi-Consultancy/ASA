14863f5f8b94177b26441ea5e17d9bf33a438601
anjaliskalange <anjaliskalange@gmail.com>
2022-01-10 14:25:53 +0530

    register final changes

--- my-app/src/components/Register/RegisterMain.js
+++ my-app/src/components/Register/RegisterMain.js
@@ -4,11 +4,13 @@ import regImage from '../../image/to-register-gb8968b104_1920.jpg';
 
 const RegisterMain = (props) => {
     return (
-        <div className='container'>
-            <div className='register__image-container'><img src={regImage} alt='registerImage' className='register__main-image'></img></div>
-            <div className='register__main-component'>
-                <div className='register__main-component_heading'>Register <span>Now</span> to become a premium member</div>
-                <Register />
+        <div className='containerR'>
+            <div className='register__image-container'>
+                <img src={regImage} alt='registerImage' className='register__main-image'></img>
+                <div className='register__main-component'>
+                    <div className='register__main-component_heading'>Register <span>Now</span> to become a premium member</div>
+                    <div className='register__main-component_form'><Register /></div>
+                </div>
             </div>
         </div>
     );
