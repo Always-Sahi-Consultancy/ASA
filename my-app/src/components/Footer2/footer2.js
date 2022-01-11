0482cbbd7b8d58a7d838e3c9adcd4921d7ac39a8
Sanket1032 <sanketshinde1032@gmail.com>
2022-01-11 22:03:03 +0530

    footer link changes

--- my-app/src/components/Footer2/footer2.js
+++ my-app/src/components/Footer2/footer2.js
@@ -1,17 +1,22 @@
 import './footer2.css';
+// import { useState } from 'react';
 import FooterTerms from './footer2_content';
 import FooterForm from './footer2_Form';
 import FooterIcon from './footer2_icon';
 
-const Footer2 = ()=>{   
-    return(
+const Footer2 = (props) => {
+    // const [active,setActive]=useState('');
+    const footerhandler=(value)=>{
+        props.onChange(value);
+    }
+    return (
         <div className='footerMain'>
             <div className='row container'>
-            <FooterForm/>
-            <FooterIcon/>
+                <FooterForm />
+                <FooterIcon />
             </div>
             <div className='container'>
-                <FooterTerms/>
+                <FooterTerms onChange={footerhandler}/>
             </div>
         </div>
     );
