6f156845b226e3bdcc4aefd3797008b1b3306d7b
Sanket1032 <sanketshinde1032@gmail.com>
2022-01-13 11:57:01 +0530

    poster subheader fixed

--- my-app/src/components/Landing/Sub header/Poster.js
+++ my-app/src/components/Landing/Sub header/Poster.js
@@ -1,15 +1,30 @@
+import React, { useState, useEffect } from 'react';
 import './Subheader.css';
 import Search from '../../../image/MG.png';
 import Image from '../../../image/Subheader_Poster1.jpg';
 
 const Poster = () => {
+    const [subheader_nav, setSubheader_nav] = useState("subheader_nav");
+    const listenScrollEvent = () => {
+        if (window.scrollY < 120) {
+            return setSubheader_nav("subheader_nav");
+        }
+        else if (window.scrollY > 120) {
+            return setSubheader_nav("subheader_fixed")
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', listenScrollEvent);
+    }, []);
+
     return (
         <div className="Poster">
             <img className="Poster_img" src={Image} />
-            <div className="subheader_nav">
+            <div className={subheader_nav}>
                 <div className="nav_tabs"><a href="#">Account & Finance</a></div>
                 <div className="nav_tabs"><a href="#">Coding & Web Development</a></div>
-                <div className="nav_tabs"><a href="#">Buisness & Freelance</a></div>
+                <div className="nav_tabs"><a href="#">Buisness & Freelancing</a></div>
                 <div className="nav_tabs"><a href="#">Video & Animation</a></div>
                 <div className="nav_tabs"><a href="#">Profile Building & Development</a></div>
             </div>
