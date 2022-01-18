e64f784d4117e3ec4853753ddf262397f45ed98f
anjaliskalange <anjaliskalange@gmail.com>
2022-01-18 16:45:20 +0530

    contactUS changes

---
+++ my-app/src/components/Dashboard/My_progress/My_progress.js
@@ -0,0 +1,30 @@
+import './My_progress.css';
+
+const MyProgress=() =>{
+    return(
+        <div>
+            <div className='main-container'>
+                <div className='progress'>
+                    <div className='progress__heading'>My Progress</div>
+                    <div className='progress__coursename'>NAME OF THE COURSE</div>
+                    <div className='progress__text1'>Access to this course will end on Date </div>
+                    <div className='range'>
+                        <div className='sliderValue'>
+                            <span>100</span>
+                        </div>
+                        <div className='field'>
+                            <div className='value left'>0</div>
+                            <input type='range' min='0' max='100' value='100' steps='1'></input>
+                            <div className='value right'>200</div>
+                        </div>
+                    </div>
+                    <button type='submit' className='progress__button'>Continue Learning</button>
+                    <div className='progress__text2'>After completing the following course you can download your certificate of completion.</div>
+                    <button type='submit' className='progress__certificate'>Download Certificate</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MyProgress;
