6f61ee2e47886a75746a19af7ba0470a752d5bff
dhanashreepatil2801 <dhanashreepatil2801@gmail.com>
2022-01-19 23:56:06 +0530

    Listview Courses

--- my-app/src/components/Dashboard/CourseGrid/CourseListview.js
+++ my-app/src/components/Dashboard/CourseGrid/CourseListview.js
@@ -2,24 +2,26 @@ import './CourseListview.css';
 import Coding from '../../../image/work-g2261fc175_1920.jpg';
 import Slider from '../../../image/courses/slider.png';
 import Arrow from '../../../image/courses/arrow.png';
-import Link from 'react-router-dom';
+// import Link from 'react-router-dom';
+import gridBlack from '../../../image/courses/gridBlack.png';
+import gridRed from '../../../image/courses/gridRed.png';
 
 const CourseListview = () => {
     const data = [
         {'image': Coding ,
-        'title': "Coding and Web Development",
+        'title': "CODING AND WEB DEVELOPMENT",
         'percentage': "45%",
         'link':"#"},
         {'image': Coding ,
-        'title': "Coding and Web Development",
+        'title': "CERTIFIED COURSE IN ACCOUNTS AND COMPLIANCE",
         'percentage': "45%",
         'link': "#"},
         {'image': Coding ,
-        'title': "Coding and Web Development",
+        'title': "CODING AND WEB DEVELOPMENT",
         'percentage': "45%",
         'link':"#"},
         {'image': Coding ,
-        'title': "Coding and Web Development",
+        'title': "CODING AND WEB DEVELOPMENT",
         'percentage': "45%",
         'link':"#"}
     ];
@@ -27,22 +29,25 @@ const CourseListview = () => {
     const Listview = (props) => {
         return(
             <div className='Listview__element' >
-                <img src={props.image}></img>
+                <img className='Listview__picture' src={props.image}></img>
                 <div className='Listview__progress'>
                     <h3>{props.title}</h3>
                     <div className='Listview__bar'>
                         <img src={Slider} alt='slider'></img>
                     </div>
                 </div>
-                <Link  to={props.link} className="Listview__continue">Continue</Link>
-                <Link  to={props.link} className="Listview__arrow"><img src={Arrow} alt='arrow'></img></Link>
+                <button className="Listview__continue">Continue</button>
+                <div className="Listview__arrow"><img src={Arrow} alt='arrow'></img></div>
             </div>
         );
     }
     return(
         <div className='Listview__container'>
             <div className='Listview__head'>
-                <div className='Listview__icon'></div>
+                <div className='Listview__icon'>
+                <img className='black' src={gridBlack}></img>
+                <img className='red' src={gridRed}></img>
+                </div>
             </div>
                 <div className='Listview__body'>
                     {data.map((item) => <Listview image={item.image} title={item.title} percentage={item.percentage} />)}
