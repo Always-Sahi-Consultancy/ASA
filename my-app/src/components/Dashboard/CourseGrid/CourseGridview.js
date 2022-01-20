d80b0dac7fe3c11822264a39a4f80a82ba06b75a
dhanashreepatil2801 <dhanashreepatil2801@gmail.com>
2022-01-20 12:38:48 +0530

    grid-listview

--- my-app/src/components/Dashboard/CourseGrid/CourseGridview.js
+++ my-app/src/components/Dashboard/CourseGrid/CourseGridview.js
@@ -5,29 +5,9 @@ import Slider from '../../../image/courses/slider.png';
 import listBlack from '../../../image/courses/listBlack.png';
 import listRed from '../../../image/courses/listRed.png';
 
-const CourseGridview = () => {
-    const data = [
-        {'image': Coding ,
-        'title': "CODING AND WEB DEVELOPMENT",
-        'percentage': "45%",
-        'link':"#"},
-        {'image': Coding ,
-        'title': "CERTIFIED COURSE IN ACCOUNTS AND COMPLIANCE",
-        'percentage': "45%",
-        'link': "#"},
-        {'image': Coding ,
-        'title': "CODING AND WEB DEVELOPMENT",
-        'percentage': "45%",
-        'link':"#"},
-        {'image': Coding ,
-        'title': "CODING AND WEB DEVELOPMENT",
-        'percentage': "45%",
-        'link':"#"}
-    ];
-
-    const Gridview = (props) => {
-        return(
-            <div className='Gridview__element' >
+const CourseGridview = (props) => {
+    return(
+        <div className='Gridview__element' >
                 <img className='Gridview__picture' src={props.image}></img>
                 <h4>{props.title}</h4>
                 <div className='Gridview__progress'>
@@ -37,20 +17,6 @@ const CourseGridview = () => {
                     <h4>{props.percentage}</h4>
                 </div>
             </div>
-        );
-    }
-    return(
-        <div className='Gridview__container'>
-            <div className='Gridview__head'>
-                <div className='Gridview__icon'>
-                <img className='black' src={listBlack}></img>
-                <img className='red' src={listRed}></img>
-                </div>
-            </div>
-                <div className='Gridview__body'>
-                    {data.map((item) => <Gridview image={item.image} title={item.title} percentage={item.percentage} />)}
-                </div>
-        </div>
     )
 };
 export default CourseGridview;
--- my-app/src/components/Dashboard/CourseGrid/CourseListview.js
+++ my-app/src/components/Dashboard/CourseGrid/CourseListview.js
@@ -1,34 +1,12 @@
 import './CourseListview.css';
-import Coding from '../../../image/work-g2261fc175_1920.jpg';
 import Slider from '../../../image/courses/slider.png';
 import Arrow from '../../../image/courses/arrow.png';
 // import Link from 'react-router-dom';
-import gridBlack from '../../../image/courses/gridBlack.png';
-import gridRed from '../../../image/courses/gridRed.png';
 
-const CourseListview = () => {
-    const data = [
-        {'image': Coding ,
-        'title': "CODING AND WEB DEVELOPMENT",
-        'percentage': "45%",
-        'link':"#"},
-        {'image': Coding ,
-        'title': "CERTIFIED COURSE IN ACCOUNTS AND COMPLIANCE",
-        'percentage': "45%",
-        'link': "#"},
-        {'image': Coding ,
-        'title': "CODING AND WEB DEVELOPMENT",
-        'percentage': "45%",
-        'link':"#"},
-        {'image': Coding ,
-        'title': "CODING AND WEB DEVELOPMENT",
-        'percentage': "45%",
-        'link':"#"}
-    ];
 
-    const Listview = (props) => {
-        return(
-            <div className='Listview__element' >
+const CourseListview = (props) => {
+    return(
+        <div className='Listview__element' >
                 <img className='Listview__picture' src={props.image}></img>
                 <div className='Listview__progress'>
                     <h3>{props.title}</h3>
@@ -39,20 +17,6 @@ const CourseListview = () => {
                 <button className="Listview__continue">Continue</button>
                 <div className="Listview__arrow"><img src={Arrow} alt='arrow'></img></div>
             </div>
-        );
-    }
-    return(
-        <div className='Listview__container'>
-            <div className='Listview__head'>
-                <div className='Listview__icon'>
-                <img className='black' src={gridBlack}></img>
-                <img className='red' src={gridRed}></img>
-                </div>
-            </div>
-                <div className='Listview__body'>
-                    {data.map((item) => <Listview image={item.image} title={item.title} percentage={item.percentage} />)}
-                </div>
-        </div>
     )
 };
 export default CourseListview;
