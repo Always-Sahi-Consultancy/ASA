33b45ed14d2b8c00d0f2937fae2adcc31b197926
Sanket1032 <sanketshinde1032@gmail.com>
2022-01-20 21:59:11 +0530

    Header Dash hamburger changes

--- my-app/src/components/Dashboard/Header/Header_Dash.js
+++ my-app/src/components/Dashboard/Header/Header_Dash.js
@@ -13,16 +13,17 @@ import DashBoardMain from "../DashBoardMain";
 const Header_Dash = () => {
     const [hamActive, setHamActive] = useState(false);
     const showMenu = () => {
+        setHamActive(!hamActive);
         const dashslider = document.getElementById('header_dash_slider');
         const hamburger = document.getElementById('header_hamburger');
-        dashslider.classList.toggle('header_dash_sliderToggle');
-        hamburger.classList.toggle('header_dash_hamburgerActive');
+        // dashslider.classList.toggle('header_dash_sliderToggle');
+        // hamburger.classList.toggle('header_dash_hamburgerActive');
         const ham_bar = document.getElementsByClassName('header_icon_bar');
         ham_bar[0].classList.toggle('header_icon_1');
         ham_bar[1].classList.toggle('header_icon_2');
         ham_bar[2].classList.toggle('header_icon_3');
     }
-    
+
     let condition = false;
     const dropdown = () => {
         const inner_dropdown = document.querySelector('.profile-inner-dropdown');
@@ -71,105 +72,105 @@ const Header_Dash = () => {
             arrow2.style.transform = "rotate(0deg)";
         }
     }
-    let condition3=false;
+    let condition3 = false;
 
-    const show = ()=>{
+    const show = () => {
         const offerRight = document.querySelector('.offer_right');
-        if(condition3===true){
-            offerRight.style.display="none";
-            condition3=true;
+        if (condition3 === true) {
+            offerRight.style.display = "none";
+            condition3 = true;
         }
-        else if(condition3==false){
-            offerRight.style.display="block";
-            condition3=false;
+        else if (condition3 == false) {
+            offerRight.style.display = "block";
+            condition3 = false;
         }
     }
 
     return (
         <div>
             <header className="Header">
-            <button className={`header_hamburger ${hamActive === true ? "ham" : ""}`} onClick={showMenu}>
-                <div className="header_icon_bar" id="header_icon_1"></div>
-                <div className="header_icon_bar" id="header_icon_2"></div>
-                <div className="header_icon_bar" id="header_icon_3"></div>
-            </button>
-            <div className="header_com_name">
-                <img className="header_com_img" src={Company}></img>
-            </div>
-            <div className="header-right">
-                <div className="header_main">
-                    <div className="header_main_tabs offer">
-                        <div className="offer_main_tab" onClick={dropdown2}>
-                            <a className="header_main_tabs" href="#">Online Courses</a>
-                            <label for="offer_courses">OFFER</label>
-                            <a href="#" id="offer_main_courses">&#9660;</a>
-                        </div>
-                        <div className="offer_tab">
-                            <div className="offer_tab_box">
-                                <div className="offer_left">
-                                    <div className="offer_left_options" id="program" onClick={show}><a href="#" className="offer_left_link">Programming</a></div>
-                                    <div className="offer_left_options"><a href="#" className="offer_left_link">Design</a></div>
-                                    <div className="offer_left_options"><a href="#" className="offer_left_link">Data Science</a></div>
-                                    <div className="offer_left_options"><a href="#" className="offer_left_link">Business and Freelance</a></div>
-                                    <div className="offer_left_options"><a href="#" className="offer_left_link">Accounts and Finance</a></div>
-                                    <div className="offer_left_options"><a href="#" className="offer_left_link">Career Development</a></div>
-                                </div>
-                                <div className="offer_right">
-                                    <div className="offer_left_options"><a href="#" className="offer_left_link">Programming with Python</a></div>
-                                    <div className="offer_left_options"><a href="#" className="offer_left_link">ReactJs</a></div>
-                                    <div className="offer_left_options"><a href="#" className="offer_left_link">Programming with C & C++</a></div>
+                <button className={`header_hamburger ${hamActive === true ? "ham header_dash_hamburgerActive" : ""}`} onClick={showMenu}>
+                    <div className="header_icon_bar" id="header_icon_1"></div>
+                    <div className="header_icon_bar" id="header_icon_2"></div>
+                    <div className="header_icon_bar" id="header_icon_3"></div>
+                </button>
+                <div className="header_com_name">
+                    <img className="header_com_img" src={Company}></img>
+                </div>
+                <div className="header-right">
+                    <div className="header_main">
+                        <div className="header_main_tabs offer">
+                            <div className="offer_main_tab" onClick={dropdown2}>
+                                <a className="header_main_tabs" href="#">Online Courses</a>
+                                <label for="offer_courses">OFFER</label>
+                                <a href="#" id="offer_main_courses">&#9660;</a>
+                            </div>
+                            <div className="offer_tab">
+                                <div className="offer_tab_box">
+                                    <div className="offer_left">
+                                        <div className="offer_left_options" id="program" onClick={show}><a href="#" className="offer_left_link">Programming</a></div>
+                                        <div className="offer_left_options"><a href="#" className="offer_left_link">Design</a></div>
+                                        <div className="offer_left_options"><a href="#" className="offer_left_link">Data Science</a></div>
+                                        <div className="offer_left_options"><a href="#" className="offer_left_link">Business and Freelance</a></div>
+                                        <div className="offer_left_options"><a href="#" className="offer_left_link">Accounts and Finance</a></div>
+                                        <div className="offer_left_options"><a href="#" className="offer_left_link">Career Development</a></div>
+                                    </div>
+                                    <div className="offer_right">
+                                        <div className="offer_left_options"><a href="#" className="offer_left_link">Programming with Python</a></div>
+                                        <div className="offer_left_options"><a href="#" className="offer_left_link">ReactJs</a></div>
+                                        <div className="offer_left_options"><a href="#" className="offer_left_link">Programming with C & C++</a></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <a className="header_main_tabs cart_link" href="#"><img className="cart" src={Cart}></img></a>
-                    <div className="Cart_box">
-                        <div className="cart_msg">Your Cart is Empty.</div>
-                        <a href="#" className="cart_button">Keep shopping</a>
-                    </div>
-                </div>
-                <div className="Header_profile">
-                    <div onClick={dropdown1} className="Header_icon">
-                        <img className="profile_photo" src={ProfilePhoto} alt="PP"></img>
-                        <a href="#" id="profile_dropdown">&#9660;</a>
-                    </div>
-                    <div className="Profile_dropdown">
-                        <div className="Profile_upper">
-                            <div className="profile_links username">USERNAME</div>
-                            <div className="profile_links user_email">user@abc.com</div>
+                        <a className="header_main_tabs cart_link" href="#"><img className="cart" src={Cart}></img></a>
+                        <div className="Cart_box">
+                            <div className="cart_msg">Your Cart is Empty.</div>
+                            <a href="#" className="cart_button">Keep shopping</a>
                         </div>
-                        <div className="Profile_middle">
-                            <div className="profile_links My_Progress"><a href="#">My Progress</a></div>
-                            <div className="profile_links Edit_Profile"><a href="#">Edit Profile</a></div>
-                            <div className="profile_links View_Dashboard"><a href="#">View Dashboard</a></div>
-                            <div className="profile_links Help_center"><a href="#">Help Center</a></div>
+                    </div>
+                    <div className="Header_profile">
+                        <div onClick={dropdown1} className="Header_icon">
+                            <img className="profile_photo" src={ProfilePhoto} alt="PP"></img>
+                            <a href="#" id="profile_dropdown">&#9660;</a>
                         </div>
-                        <div className="Profile_lower">
-                            <div className="profile_links My_Progress"><a href="#">Log Out</a></div>
-                            <div className="profile_links Manage_account" onClick={dropdown}>
-                                <a href="#">Manage Account</a>
-                                <a href="#" id="offer_courses">&#9660;</a></div>
-                            <ul className="profile-inner-dropdown">
-                                <li><a className="manage_dropdown" href="#">Change Password</a></li>
-                                <li><a className="manage_dropdown" href="#">Change Email Address</a></li>
-                                <li><a className="manage_dropdown" href="#">Switch Account</a></li>
-                                <li><a className="manage_dropdown" href="#">Delete Account</a></li>
-                            </ul>
-                        </div>
+                        <div className="Profile_dropdown">
+                            <div className="Profile_upper">
+                                <div className="profile_links username">USERNAME</div>
+                                <div className="profile_links user_email">user@abc.com</div>
+                            </div>
+                            <div className="Profile_middle">
+                                <div className="profile_links My_Progress"><a href="#">My Progress</a></div>
+                                <div className="profile_links Edit_Profile"><a href="#">Edit Profile</a></div>
+                                <div className="profile_links View_Dashboard"><a href="#">View Dashboard</a></div>
+                                <div className="profile_links Help_center"><a href="#">Help Center</a></div>
+                            </div>
+                            <div className="Profile_lower">
+                                <div className="profile_links My_Progress"><a href="#">Log Out</a></div>
+                                <div className="profile_links Manage_account" onClick={dropdown}>
+                                    <a href="#">Manage Account</a>
+                                    <a href="#" id="offer_courses">&#9660;</a></div>
+                                <ul className="profile-inner-dropdown">
+                                    <li><a className="manage_dropdown" href="#">Change Password</a></li>
+                                    <li><a className="manage_dropdown" href="#">Change Email Address</a></li>
+                                    <li><a className="manage_dropdown" href="#">Switch Account</a></li>
+                                    <li><a className="manage_dropdown" href="#">Delete Account</a></li>
+                                </ul>
+                            </div>
 
-                    </div >
+                        </div >
+                    </div>
                 </div>
-            </div>
-            <div className="header_dash_slider" id="header_dash_slider" onClick={showMenu}>
-            <div className="header_dash_sliderOption">
-                <div className="header_dash_option">Home</div>
-                <div className="header_dash_option">Courses</div>
-                <div className="header_dash_option">My Cart</div>
-                <div className="header_dash_option">User Profile</div>
-            </div>
-        </div>
-        </header >
-        <DashBoardMain/>
+                <div className={`header_dash_slider ${hamActive ? "header_dash_sliderToggle" : ""}`} id="header_dash_slider" onClick={showMenu}>
+                    <div className="header_dash_sliderOption">
+                        <div className="header_dash_option">Home</div>
+                        <div className="header_dash_option">Courses</div>
+                        <div className="header_dash_option">My Cart</div>
+                        <div className="header_dash_option">User Profile</div>
+                    </div>
+                </div>
+            </header >
+            <DashBoardMain />
         </div>
     );
 };
