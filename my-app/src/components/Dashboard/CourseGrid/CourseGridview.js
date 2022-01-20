a352bd965106570f182022d4ecf0b770bdefc889
Sanket1032 <sanketshinde1032@gmail.com>
2022-01-20 21:25:53 +0530

    Course Grid Changes

--- my-app/src/components/Dashboard/CourseGrid/CourseGridview.js
+++ my-app/src/components/Dashboard/CourseGrid/CourseGridview.js
@@ -1,58 +1,36 @@
 import './Coursegridview.css';
-import Coding from '../../../image/work-g2261fc175_1920.jpg';
 import Slider from '../../../image/courses/slider.png';
-// import Link from "react-router-dom";
-import listBlack from '../../../image/courses/listBlack.png';
-import listRed from '../../../image/courses/listRed.png';
 
-const CourseGridview = () => {
+const CourseGridview = (props) => {
 
-    const data = [
-        {'image': Coding ,
-        'title': "CODING AND WEB DEVELOPMENT",
-        'percentage': "45%",
-        'link':"#"},
-        {'image': Coding ,
-        'title': "CERTIFIED COURSE IN ACCOUNTS AND COMPLIANCE",
-        'percentage': "45%",
-        'link': "#"},
-        {'image': Coding ,
-        'title': "CODING AND WEB DEVELOPMENT",
-        'percentage': "45%",
-        'link':"#"},
-        {'image': Coding ,
-        'title': "CODING AND WEB DEVELOPMENT",
-        'percentage': "45%",
-        'link':"#"}
-    ];
+    
 
-    const GridView = (props) => {
-        return(
-            <div className='Gridview__element' >
-                <img className='Gridview__picture' src={props.image}></img>
-                <h4>{props.title}</h4>
-                <div className='Gridview__progress'>
-                    <div className='Gridview__bar'>
-                        <img src={Slider} alt='slider'></img>
-                    </div>
-                    <h4>{props.percentage}</h4>
+    // const GridView = (props) => {
+    //     return (
+            
+    //     )
+    // };
+    // const GridIconHandler = () => {
+    //     const Gridview = document.querySelector(".Gridview__container");
+    //     if (props.onChange === true) {
+    //         Gridview.style.display = "block";
+    //     }
+    //     else if (props.onChange === false) {
+    //         Gridview.style.display = "none";
+    //     }
+    // }
+
+    return (
+        <div className='Gridview__element' >
+            <img className='Gridview__picture' src={props.image}></img>
+            <h4>{props.title}</h4>
+            <div className='Gridview__progress'>
+                <div className='Gridview__bar'>
+                    <img src={Slider} alt='slider'></img>
                 </div>
-        </div>
-        )
-    };
-
-    return(
-        <div className="Gridview__container">
-        <div className='Gridview__head'>
-            <div className='Gridview__icon'>
-            <img className='black' src={listBlack}></img>
-            <img className='red' src={listRed}></img>
+                <h4>{props.percentage}</h4>
             </div>
         </div>
-            <div className='Gridview__body'>
-                {data.map((item) => <GridView image={item.image} title={item.title} percentage={item.percentage} />)}
-            </div>
-    </div> 
     )
 };
 export default CourseGridview;
