43229871e90b156d8f68be492418482e6fe924f7
MandeepDalavi <mandeepdalavi@gmail.com>
2022-01-27 12:08:31 +0530

    UserContext, useContext & Reducer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState, createContext, useReducer } from 'react';
 import './css/root2.css';
 import TermsOfUse from './components/Terms/TermsOfUse';
 import EarningDiscalimer from './components/Terms/EarningDisclaimer';
@@ -19,44 +19,59 @@ import Accounting from './components/IndividualCourse/Accounting';
 import Error404 from './components/404Error/404Error';
 import Dash from './components/Dashboard/dashboard/Dash';
 
-import {BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import {initialState, reducer} from './reducer/UseReducer';
+
+import {Link, Routes, Route } from 'react-router-dom';
 import DashBoardMain from './components/Dashboard/DashBoardMain';
 import VideoModule from './components/Dashboard/VideoModule/VideoModule';
 // import VideoContent from './components/Dashboard/VideoModule/VideoContent';
 import VideoPlayer from './components/Dashboard/VideoModule/VideoPlayer';
-  
-function App() {
-  const courses = ["Web-development-for-everyone","CERTIFIED-COURSE-IN-FINANCIAL-ACCOUNTING", "A-FREE-TOOLKIT-FOR-18+","FUNDAMENTALS-OF-FINANCIAL-INTELLIGENCE", "NAVRATNAS-OF-TIME-MANAGEMENT","HOW-TO-FIND-YOUR-PASSION"];
+
+export const UserContext = createContext();
+
+const courses = ["Web-development-for-everyone","CERTIFIED-COURSE-IN-FINANCIAL-ACCOUNTING", "A-FREE-TOOLKIT-FOR-18+","FUNDAMENTALS-OF-FINANCIAL-INTELLIGENCE", "NAVRATNAS-OF-TIME-MANAGEMENT","HOW-TO-FIND-YOUR-PASSION"];
+
+const Routing = () => {
   return (
     <div>
-      <BrowserRouter>
-      {/* <Signin /> */}
-      <Header2 />
-        <Link to="/RegisterNow"></Link>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/testimonials" element={<Testimonials />} />
-          <Route path="/contact_us" element={<ContactUs />} />
-          <Route path="/about_us" element={<OurStroy />} />
-          <Route path="/RegisterNow" element={<RegisterMain />} />
-          <Route path="/CourseName" element={<Accounting />} />
-          <Route path="/TermsofUse" element={<TermsOfUse />} />
-          <Route path="/EarningDisclaimer" element={<PrivacyPolicy />} />
-          <Route path="/RefundPolicy" element={<RefundPolicy />} />
-          <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
-          <Route path="CoursePage" element={<CoursePage />} />
-          <Route path={"/dash"} element={<Dash/> } />
-          <Route path={"/DashBoard"} element={<DashBoardMain />} />
-          <Route path={"/Player"} element={<VideoModule/>} />
-          {courses.forEach(element => {
-            var elements = "/"+element;
-            <Route path={elements} element={<Accounting />} />
-          })}
-          <Route path="*" element={<Error404 />} />
-          <Route path="/logout" element={<Logout />} />
-        </Routes>
+      <Signin />
+      <Link to="/RegisterNow"></Link>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+        <Route path="/contact_us" element={<ContactUs />} />
+        <Route path="/about_us" element={<OurStroy />} />
+        <Route path="/RegisterNow" element={<RegisterMain />} />
+        <Route path="/CourseName" element={<Accounting />} />
+        <Route path="/TermsofUse" element={<TermsOfUse />} />
+        <Route path="/EarningDisclaimer" element={<PrivacyPolicy />} />
+        <Route path="/RefundPolicy" element={<RefundPolicy />} />
+        <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+        <Route path="/CoursePage" element={<CoursePage />} />
+        <Route path={"/dash"} element={<Dash/> } />
+        <Route path={"/DashBoard"} element={<DashBoardMain />} />
+        <Route path={"/Player"} element={<VideoModule/>} />
+        {courses.forEach(element => {
+          var elements = "/"+element;
+          <Route path={elements} element={<Accounting />} />
+        })}
+        <Route path="*" element={<Error404 />} />
+      </Routes>
       <Footer2 />
-      </BrowserRouter>
+    </div>
+  );
+} 
+  
+function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <UserContext.Provider value={{state, dispatch}}>
+          <Header2 />
+          <Routing/>
+      </UserContext.Provider>
     </div>
   );
 }
