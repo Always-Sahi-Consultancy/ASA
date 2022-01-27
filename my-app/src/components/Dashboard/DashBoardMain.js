4e7468974d9102db05a58295a872888572dbf4f8
Akhil-A0402 <akhil.a.nair760@gmail.com>
2022-01-27 11:58:51 +0530

    Changes

--- my-app/src/components/Dashboard/DashBoardMain.js
+++ my-app/src/components/Dashboard/DashBoardMain.js
@@ -2,11 +2,24 @@
 import './DashBoardMain.css';
 import MyProgress from './My_progress/My_progress';
 import CourseGrid from './CourseGrid/CourseGrid';
+import { BrowserRouter, Router, Route, Link} from 'react-router-dom';
+import Accounting from '../IndividualCourse/Accounting';
 const DashBoardMain = () =>{
-    return(
-        <div className='dashs-container'>
-            <MyProgress/>
-            <CourseGrid/>
+    const data = [{'courseName': 'Web Developement for Everyone','path': 'web_developement_for_everyone', 'page':<Accounting />}]
+
+        return(
+        <div>
+            <BrowserRouter>
+                <Router>
+                {data.map(elements => (<Link to={'Dashboard/'+elements.path} />))}
+
+                </Router>
+            </BrowserRouter>
+
+            <div className='dashs-container'>
+                <MyProgress/>
+                <CourseGrid/>
+            </div>
         </div>
     );
 }
