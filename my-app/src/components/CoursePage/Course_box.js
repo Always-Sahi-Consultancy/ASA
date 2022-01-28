556f489be3674758171c013dc26bfbd98acd8ece
Akhil-A0402 <akhil.a.nair760@gmail.com>
2022-01-28 09:33:28 +0530

    Course dynamic

--- my-app/src/components/CoursePage/Course_box.js
+++ my-app/src/components/CoursePage/Course_box.js
@@ -1,25 +1,41 @@
 import './Courses.css';
 import Course_rate from './Course_rate';
+import Account_Compliance from '../../image/Certified Course in Accounts & Compliance.jpg';
+import taxation from '../../image/Certified Course in Taxation.jpg';
+import Financial_Accounting from '../../image/Certified Course in Financial Accounting.png';
+import Financial_intelligence from '../../image/Fundamentals of Financial Intelligence.png';
+import Advance_financial from '../../image/Advance Course in Financial Intelligence.jpg';
+import Web_image from '../../image/web development.jpg';
+
+const CourseImage = {
+    "Account_Compliance": Account_Compliance,
+    "taxation": taxation,
+    "Financial_Accounting": Financial_Accounting,
+    "Financial_intelligence": Financial_intelligence,
+    "Advance_financial": Advance_financial,
+    "Web_image": Web_image
+}
 
 const Course_box = (props) => {
     // "Buy_button buy"
     return (
         <div className="course_box">
-            <div className="course_img"><img className="course_main_img" src={props.image}></img></div>
+            <div className="course_img"><img className="course_main_img" src={CourseImage[props.courseImage]} alt={props.courseImage}></img></div>
             <div className="course_details">
-                <h2 className="course_title">{props.title}</h2>
+                <h2 className="course_title">{props.courseName}</h2>
+                {console.log(props.courseImage)}
                 <div className="course_details_main">
                     <div className="course_details_upper">
-                        <Course_rate star={props.star} />
-                        <div className="Prof_name">{props.Prof_name}</div>
+                        <Course_rate star={props.courseStarRate} />
+                        <div className="Prof_name">{props.courseTutor}</div>
                         <div className="cost_section">
-                            <div className="course_cost">{props.course_cost}</div>
-                            <div className="original_cost"><s>{props.ori_cost}</s></div>
+                            <div className="course_cost">{props.courseDiscountPrice}</div>
+                            <div className="original_cost"><s>{props.courseMRP}</s></div>
                         </div>
                     </div>
                     <div className="Course_button">
-                        <a href="#" className={`Buy_button buy ${props.index === "1" ? "free_button" : ""}`}>{props.buy_now}</a>
-                        <a href="#" className="Buy_button">{props.about_course}</a>
+                        <a href="#" className={`Buy_button buy ${props.courseMRP < "0" ? "free_button" : ""}`}>Buy Now</a>
+                        <a href="#" className="Buy_button">About Course</a>
                     </div>
                 </div>
             </div>
---
+++ my-app/src/components/CoursePage/courseData.js
@@ -0,0 +1,272 @@
+const course_details = [
+    {
+        image: webDevelopment,
+        title: "Web Development For Everyone",
+        stars: [
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            }
+        ],
+        index: "0",
+        Prof_name: "Atharv",
+        course_cost: "Rs. 2999",
+        Original_cost: "Rs. 5000",
+        buy_now: "Buy Now",
+        about_course: "About Course"
+    },
+    {
+        image: Financial_Accounting,
+        title: "Certified Course in Financial Accounting",
+        stars: [
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            }
+        ],
+        index: "0",
+        Prof_name: "Atharv",
+        course_cost: "Rs. 2999",
+        Original_cost: "Rs. 8000",
+        buy_now: "Buy Now",
+        about_course: "About Course"
+    },
+    {
+        image: tootkit,
+        title: "A Free toolkit for 18+",
+        stars: [
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star
+            }
+        ],
+        index: "1",
+        Prof_name: "Atharv",
+        course_cost: "",
+        Original_cost: "",
+        buy_now: "Free",
+        about_course: "Get it"
+    },
+    {
+        image: Financial_intelligence,
+        title: "Fundamentals of Financial Intelligence",
+        stars: [
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star
+            }
+        ],
+        index: "0",
+        Prof_name: "Atharv",
+        course_cost: "Rs. 599",
+        Original_cost: "Rs. 2000",
+        buy_now: "Buy Now",
+        about_course: "About Course"
+    },
+    {
+        image: Navratnas,
+        title: "Navratnas of Time Management",
+        stars: [
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star
+            }
+        ],
+        index: "1",
+        Prof_name: "Atharv",
+        course_cost: "",
+        Original_cost: "",
+        buy_now: "Free",
+        about_course: "About Course"
+    },
+    {
+        image: Passion,
+        title: "How to Find Your Passion",
+        stars: [
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star
+            }
+        ],
+        index: "1",
+        Prof_name: "Atharv",
+        course_cost: "",
+        Original_cost: "",
+        buy_now: "Free",
+        about_course: "About Course"
+    },
+    {
+        image: Passion,
+        title: "Create your LindedIn Profile",
+        stars: [
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star
+            }
+        ],
+        index: "1",
+        Prof_name: "Atharv",
+        course_cost: "",
+        Original_cost: "",
+        buy_now: "Free",
+        about_course: "About Course"
+    },
+    {
+        image: Advance_financial,
+        title: "Advance Course in Financial Intelligence",
+        stars: [
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star
+            }
+        ],
+        index: "0",
+        Prof_name: "Atharv",
+        course_cost: "Rs. 999",
+        Original_cost: "Rs. 1000",
+        buy_now: "Buy Now",
+        about_course: "About Course"
+    },
+    {
+        image: Account_Compliance,
+        title: "Certified Course in Accounts & Compliance",
+        stars: [
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star
+            }
+        ],
+        index: "0",
+        Prof_name: "Atharv",
+        course_cost: "Rs. 999",
+        Original_cost: "Rs. 2000",
+        buy_now: "Buy Now",
+        about_course: "About Course"
+    },
+    {
+        image: taxation,
+        title: "Certified Course in Taxation",
+        stars: [
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star1
+            },
+            {
+                star: star
+            }
+        ],
+        index: "0",
+        Prof_name: "Atharv",
+        course_cost: "Rs. 1999",
+        Original_cost: "Rs. 4000",
+        buy_now: "Buy Now",
+        about_course: "About Course"
+    },
+];
