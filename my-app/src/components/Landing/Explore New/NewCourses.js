9e8d14055c83535cae6ea7a6d2cfb42593782412
Akhil-A0402 <akhil.a.nair760@gmail.com>
2022-02-02 11:02:38 +0530

    Linking

--- my-app/src/components/Landing/Explore New/NewCourses.js
+++ my-app/src/components/Landing/Explore New/NewCourses.js
@@ -1,15 +1,18 @@
+import {Link} from 'react-router-dom';
 import './NewCourses.css';
 
 
 function NewCourses(props) {
 
     return <div>
+        {/* <Link to={props.link}> */}
         <div className='content'>
             <a className='containerNC'>
                 <div className='element'><img src={props.Image} className='element_img'></img></div>
                 <div className='element_name'>{props.Text}</div>
             </a>
         </div>
+        {/* </Link>  */}
     </div>
 }
 
