a1619c76f88b31c40c806ec68fd3c900f6f7f55f
Akhil-A0402 <akhil.a.nair760@gmail.com>
2022-02-03 13:37:08 +0530

    Course

--- my-app/src/components/IndividualCourse/Accounting.js
+++ my-app/src/components/IndividualCourse/Accounting.js
@@ -3,21 +3,53 @@ import Objective from './objective/objective';
 import Highlights from './Highlights/Highlights';
 import AccountingContent from './Accounting/accountingcontent';
 import YourTeacher from './YourTeachers/YourTeachers';
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import {UserContext} from '../../App';
 
+import * as CourseData from './CourseData.json';
+
 const Course =() =>{
+
+    const [isloaded,setloaded] = useState(false);
+
+    let courseInfo;
+    console.log(courseInfo);
+
+    let courseName, courseData;
+    const data = async (pathname) => {
+        courseInfo= CourseData.default[pathname];
+        console.log(courseInfo);
+        courseName= courseInfo["courseName"];
+        console.log(courseName);
+        courseData = courseInfo["courseData"];
+        console.log(courseData);
+        if(courseData != undefined){
+            setloaded(true);
+            // console.log(isloaded);
+        }
+    }
+
+    useEffect(() => {
+        data(window.location.pathname)
+    },[]);
+
+    const Course =  <div>
+                        <Banner />
+                        {console.log(courseName)}
+                        <Objective/>
+                        <Highlights/>
+                        <div>
+                            <AccountingContent/>
+                        </div>
+                        <YourTeacher/>
+                    </div>
+
     const {state, dispatch} = useContext(UserContext);
     return(
         <div>
-            <Banner/>
-            <Objective/>
-            <Highlights/>
-            <div>
-                <AccountingContent/>
-            </div>
-            <YourTeacher/>
+            {isloaded? Course:null}
         </div>
+        
     );
 }
 
