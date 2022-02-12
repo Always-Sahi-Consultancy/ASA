37be022c5da4ceb326c6701c61093efcc314607b
MandeepDalavi <mandeepdalavi@gmail.com>
2022-02-12 19:48:20 +0530

    Video Module Data fetch

--- my-app/src/components/Dashboard/VideoModule/VideoMain.js
+++ my-app/src/components/Dashboard/VideoModule/VideoMain.js
@@ -1,8 +1,46 @@
+import { useEffect, useState } from "react";
 import VideoModule from "./VideoModule";
 import VideoPlayer from "./VideoPlayer";
 import './VideoMain.css';
+import { useContext } from "react";
+import { UserContext } from "../../../App";
+import { useNavigate } from "react-router-dom";
 
 const VideoMain =()=>{
+
+    const { state, dispatch } = useContext(UserContext);
+
+    const navigate = useNavigate();
+
+    const [data, setData] = useState({});
+
+    const callData = async () => {
+        try {
+            const res = await fetch("/module", {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                }
+            });
+
+            const data = await res.json();
+            console.log(data);
+            setData(data);
+            if (!res.status === 200) {
+                const error = new Error(res.error);
+                throw error;
+            }
+        } catch (err) {
+            console.log(err);
+            navigate("/");
+        }
+    }
+
+    useEffect(() => {
+        callData();
+    }, []);
+
     return(
         <div className="video-row">
             <VideoModule/>
