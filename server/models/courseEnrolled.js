610dc2e5a89d4a8825f3ada1cfb4c5b2e48f336b
Akhil-A0402 <akhil.a.nair760@gmail.com>
2022-02-17 21:13:09 +0530

    Schema

--- server/models/courseEnrolled.js
+++ server/models/courseEnrolled.js
@@ -5,21 +5,23 @@ const courseEnrolledSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    courseEnrolled: {
-        courseName: {
-            type: String,
-            required: true
-        },
-        dateOfEnrollmnent: {
-            type: Date,
-            required: true
-        },
-        videoStatus: [
-            {
-                type: Number
-            }
+    courseEnrolled: [
+        {
+            courseName: {
+                type: String,
+                required: true
+            },
+            dateOfEnrollmnent: {
+                type: Date,
+                required: true
+            },
+            videoStatus: [
+                {
+                    type: Number
+                }
         ]
-    }
+        }
+    ]
 });
 
 const courseEnrolled = mongoose.model('COURSEENROLLED', courseEnrolledSchema);
