ed367844221b7bca0cef39eda5496ab69676074b
Sanket1032 <sanketshinde1032@gmail.com>
2022-02-18 17:40:35 +0530

    video player dropdown

--- my-app/src/components/Dashboard/VideoModule/VideoMain.js
+++ my-app/src/components/Dashboard/VideoModule/VideoMain.js
@@ -3,9 +3,12 @@ import { BrowserRouter, useLocation } from "react-router-dom";
 import VideoModule from "./VideoModule";
 import VideoPlayer from "./VideoPlayer";
 import './VideoMain.css';
+import VideoContent from './VideoContent';
 import { useHistory } from "react-router-dom";
+import React, { useContext } from 'react';
+import { UserContext } from '../../../App';
 
-const VideoMain =()=>{
+const VideoMain = (props) => {
     const [videoData, setVideo] = useState("");
     const [courseName, setCourse] = useState("");
 
@@ -20,70 +23,70 @@ const VideoMain =()=>{
     const [nextVideo, setNext] = useState("");
     const [prevVideo, setPrev] = useState("");
 
-    const data = async (course_name) =>{
+    const data = async (course_name) => {
         course_name = course_name.split("/")[2];
-        try{
-        const res = await fetch("/moduledata", {
-            method: "POST",
-            headers: {
-                "Content-Type" : "application/json"
-            },
-            body: JSON.stringify({course_name})
-        });
-        const data = await res.json();
-        // console.log(data);
-        if(res.status != 200){
-            const error = new Error(res.error);
-            throw error;
+        try {
+            const res = await fetch("/moduledata", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ course_name })
+            });
+            const data = await res.json();
+            // console.log(data);
+            if (res.status != 200) {
+                const error = new Error(res.error);
+                throw error;
+            }
+            setModules(data["courseIndex"]);
+            setCourse(data["courseName"]);
+            setModule(true);
         }
-        setModules(data["courseIndex"]);
-        setCourse(data["courseName"]);
-        setModule(true);
-    }
-    catch (error) {
-        console.log(error);
-    }
+        catch (error) {
+            console.log(error);
+        }
     }
 
-    const videodata = async (course_name) =>{
+    const videodata = async (course_name) => {
         course_name = course_name.split("/")[2];
-        try{
-        const res = await fetch("/videodetails", {
-            method: "POST",
-            headers: {
-                "Content-Type" : "application/json"
-            },
-            body: JSON.stringify({course_name})
-        });
-        const data = await res.json();
-        // console.log(data);
-        setVideo(data);
-        setVideos(true);
-        if(res.status != 200){
-            const error = new Error(res.error);
-            throw error;
+        try {
+            const res = await fetch("/videodetails", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ course_name })
+            });
+            const data = await res.json();
+            // console.log(data);
+            setVideo(data);
+            setVideos(true);
+            if (res.status != 200) {
+                const error = new Error(res.error);
+                throw error;
+            }
         }
-    }
-    catch (error) {
-        console.log(error);
-    }
+        catch (error) {
+            console.log(error);
+        }
     }
 
     const module_topic = (video) => {
         const viurl = window.location.pathname.split("/")[3];
         // console.log(video["videoName"].split(" ").join("")==viurl)
-         if(video["videoName"].split(" ").join("") == viurl){
-             return video;
-         }
+        if (video["videoName"].split(" ").join("") == viurl) {
+            return video;
+        }
     }
 
     const currentMoudle = () => {
         const moduleName = window.location.pathname.split('/')[3];
-        if(moudlestate && videostate){
+        if (moudlestate && videostate) {
             const [currentVideo] = videoData["courseVideos"].filter(module_topic);
-            if(currentVideo!=undefined)
+            if (currentVideo != undefined)
                 setCurrent(currentVideo);
-            else{
+            else {
                 window.location.pathname = "404eroor";
             }
         }
@@ -109,13 +112,32 @@ const VideoMain =()=>{
     //     }
     // }
 
-    useEffect(() => {data(window.location.pathname); videodata(window.location.pathname);},[location]);
-    useEffect(() => {currentMoudle();});
+    useEffect(() => { data(window.location.pathname); videodata(window.location.pathname); }, [location]);
+    useEffect(() => { currentMoudle(); });
+    const { state, dispatch } = useContext(UserContext);
+    // const videoData = props.data;
+    const [videoData1, setVideo1] = useState([]);
 
-    return(
-        <div className="video-row">
-            <VideoModule courseIndex={modules} courseName={courseName}/>
-            <VideoPlayer key={currentVideo._id} title={currentVideo.videoName} url={currentVideo.videoURL} material={currentVideo.videoResources} transcript={currentVideo.videoTranscript} />
+    useEffect(() => { setVideo1(modules) })
+    const [videoContainerVisible,setVideoContainerVisible]=useState(false);
+    const videoContainer=()=>{
+        setVideoContainerVisible(!videoContainerVisible);
+    }
+    return (
+        <div>
+            <div className="module" onClick={videoContainer}>Module
+                <div className={`video-container1 ${videoContainerVisible ? "setVisible" : "setNotVisible"}`}>
+                    {
+                        videoData1.map((item) =>
+                            <VideoContent name={item.name} id={item.id} details={item.details} video={courseName} />
+                        )
+                    }
+                </div>
+            </div>
+            <div className="video-row">
+                <VideoModule courseIndex={modules} courseName={courseName} />
+                <VideoPlayer key={currentVideo._id} title={currentVideo.videoName} url={currentVideo.videoURL} material={currentVideo.videoResources} transcript={currentVideo.videoTranscript} />
+            </div>
         </div>
     );
 }
