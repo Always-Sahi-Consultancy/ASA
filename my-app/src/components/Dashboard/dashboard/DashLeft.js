ce42de1684c2ae864afc7b27acc728a959927937
Sanket1032 <sanketshinde1032@gmail.com>
2022-02-18 20:15:03 +0530

    dashLeft changes

--- my-app/src/components/Dashboard/dashboard/DashLeft.js
+++ my-app/src/components/Dashboard/dashboard/DashLeft.js
@@ -40,23 +40,24 @@ const DashLeft = (props) => {
 
     // Logout
     const logout = async () => {
-        try{
+        try {
             console.log("Inside");
             const res = await fetch('/logout', {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
             });
-            if(res.status == 200 ){
-            console.log(res);
-            alert("Logout Successful");
-            navigate("/")
+            if (res.status == 200) {
+                console.log(res);
+                alert("Logout Successful");
+                navigate("/")
             }
-            else{
+            else {
                 alert("Logout Unsuccessful");
-            }}
-        catch(error){
+            }
+        }
+        catch (error) {
             console.log(error);
         }
     }
@@ -66,19 +67,35 @@ const DashLeft = (props) => {
         callDashPage();
     }, []);
     //  ${ progress ? 'show_tab' : 'hide_tab' }
+    const [dashLeft, setdashLeft] = useState(false);
+    const dashLeftHandler = () => {
+        const dash_downarrow = document.querySelector('.dash_downarrow');
+        setdashLeft(!dashLeft);
+        if (dashLeft) {
+            dash_downarrow.style.transform = "rotate(0deg)";
+        }
+        else {
+            dash_downarrow.style.transform = "rotate(180deg)";
+        }
+    }
     return (
         <div className='dashleft-container'>
-            <div className='dash__icon'>M</div>
-            <form method="GET">
-                <div className='dash__name'>{userData.userFirstName + " " + userData.userLastName}</div>
-            </form>
-            <div className='dash__tabs'>
-                <div className='dash__progress dash__tab' onClick={() => { props.setPage("Myprogress") }}>My Progress</div>
-                <div className='dash__profile dash__tab' onClick={() => { props.setPage("EditProfile") }}>Edit Profile</div>
-                <div className='dash__view dash__tab'>View Dash Board</div>
-                <div className='dash__help dash__tab' onClick={() => { props.setPage("helpCenter") }}>Help Centre</div>
-                <div className='dash__password dash__tab' onClick={() => { props.setPage("ChangePassword") }}>Change Password</div>
-                <div className='dash__logout dash__tab' onClick={logout} >Logout</div>
+            <div className="dashLeft_icon">
+                <div className='dash__icon'>M</div>
+                <div className="dash_downarrow" onClick={dashLeftHandler}>&#x2193;</div>
+            </div>
+            <div className={`${dashLeft ? "dashLeftVisible" : "dashLeftNone"}`}>
+                <form method="GET">
+                    <div className='dash__name'>{userData.userFirstName + " " + userData.userLastName}</div>
+                </form>
+                <div className='dash__tabs'>
+                    <div className='dash__progress dash__tab' onClick={() => { props.setPage("Myprogress") }}>My Progress</div>
+                    <div className='dash__profile dash__tab' onClick={() => { props.setPage("EditProfile") }}>Edit Profile</div>
+                    <div className='dash__view dash__tab'>View Dash Board</div>
+                    <div className='dash__help dash__tab' onClick={() => { props.setPage("helpCenter") }}>Help Centre</div>
+                    <div className='dash__password dash__tab' onClick={() => { props.setPage("ChangePassword") }}>Change Password</div>
+                    <div className='dash__logout dash__tab' onClick={logout} >Logout</div>
+                </div>
             </div>
         </div>
     );
